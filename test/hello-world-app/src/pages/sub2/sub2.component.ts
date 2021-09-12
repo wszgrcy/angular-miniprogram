d94d9d117e36d52572a6d68234925b2deb9257d4
HyperLife1119 <hyperlife1119@qq.com>
2021-09-13 00:06:58 +0800

    fix: 调整生命周期钩子用法

--- test/hello-world-app/src/pages/sub2/sub2.component.ts
+++ test/hello-world-app/src/pages/sub2/sub2.component.ts
@@ -1,51 +1,57 @@
 import { Component, OnInit } from '@angular/core';
-import { WxLifetimes } from '../../../../../src/platform';
 
 @Component({
   selector: 'app-sub2',
   templateUrl: './sub2.component.html',
   styleUrls: ['./sub2.component.css'],
 })
-export class Sub2Component implements OnInit, WxLifetimes {
+export class Sub2Component implements OnInit {
   title = 'sub2组件';
-  wxLifetimes: WechatMiniprogram.Component.Lifetimes['lifetimes'] = {
-    attached: () => {
-      console.log('attached', this);
-    },
-    ready: () => {
-      console.log('ready', this);
-    },
-    moved: () => {
-      console.log('moved', this);
-    },
-    detached: () => {
-      console.log('detached', this);
-    },
-    error: (err) => {
-      console.log(err);
 
-      console.log('error', this);
-    },
-  };
-  wxPageLifetimes: Partial<WechatMiniprogram.Component.PageLifetimes> = {
-    show: () => {
-      console.log('page-show', this);
-    },
-    hide: () => {
-      console.log('page-hide', this);
-    },
-    resize: () => {
-      console.log('page-resize', this);
-    },
-  };
   constructor() {}
+
+  attached() {
+    console.log('attached', this);
+  }
+
+  ready() {
+    console.log('ready', this);
+  }
+
+  moved() {
+    console.log('moved', this);
+  }
+
+  detached() {
+    console.log('detached', this);
+  }
+
+  error(err) {
+    console.log(err);
+
+    console.log('error', this);
+  }
+
+  show() {
+    console.log('page-show', this);
+  }
+
+  hide() {
+    console.log('page-hide', this);
+  }
+
+  resize() {
+    console.log('page-resize', this);
+  }
 
   ngOnInit() {
     console.log('created-ngOnInit', this);
   }
+
   testApp($event) {
     console.log('sub2事件', $event);
   }
+
   ngOnDestroy(): void {
     console.log('detached-ngOnDestroy', this);
   }
