b220f384a57dd7b7648e70937b4a6d97742a2c84
wszgrcy <wszgrcy@gmail.com>
2021-12-29 08:31:26 +0800

    refactor: 重构

--- src/builder/token/component.token.ts
+++ src/builder/token/component.token.ts
@@ -1,13 +1,3 @@
 import { InjectionToken } from 'static-injector';
 
-export const COMPONENT_FILE_NAME_TOKEN = new InjectionToken<string>(
-  'COMPONENT_FILE_NAME_TOKEN'
-);
-export const COMPONENT_TEMPLATE_CONTENT_TOKEN = new InjectionToken<string>(
-  'COMPONENT_TEMPLATE_CONTENT_TOKEN'
-);
-export const TEMPLATE_COMPILER_OPTIONS_TOKEN = new InjectionToken(
-  'TEMPLATE_COMPILER_TOKEN'
-);
-
 export const COMPONENT_META = new InjectionToken('COMPONENT_META');
