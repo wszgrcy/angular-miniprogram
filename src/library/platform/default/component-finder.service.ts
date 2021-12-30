cbe325e817be711e8122429f371b583102a829b0
wszgrcy <wszgrcy@gmail.com>
2021-12-30 20:02:48 +0800

    fix: component-finder读取

--- src/library/platform/default/component-finder.service.ts
+++ src/library/platform/default/component-finder.service.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Subject, from, merge } from 'rxjs';
+import { filter, map, take } from 'rxjs/operators';
 
 @Injectable()
-export class ComponentFinderService<T> {
+export class ComponentFinderService<T = unknown> {
   private map = new Map<unknown, T>();
+  private subject = new Subject<[unknown, T]>();
   get(component: unknown) {
-    return this.map.get(component);
+    return merge(from(this.map), this.subject).pipe(
+      filter((item) => item[0] === component),
+      map((item) => item[1]),
+      take(1)
+    );
   }
   set(component: unknown, instance: T) {
+    this.subject.next([component, instance]);
     return this.map.set(component, instance);
   }
   remove(component: unknown) {
