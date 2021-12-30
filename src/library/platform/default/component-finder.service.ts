4cf07d5da731bcc09f553161885548001cb35368
wszgrcy <wszgrcy@gmail.com>
2021-12-30 15:16:51 +0800

    refactor: library/platform

--- src/library/platform/default/component-finder.service.ts
+++ src/library/platform/default/component-finder.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Type } from '@angular/core';
+import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ComponentFinderService<T> {
--- src/library/platform/default/token.ts
+++ src/library/platform/default/token.ts
@@ -1,3 +1,3 @@
 import { InjectionToken } from '@angular/core';
 
-export const PAGE_TOKEN = new InjectionToken<any>('PAGE_TOKEN');
+export const PAGE_TOKEN = new InjectionToken<unknown>('PAGE_TOKEN');
