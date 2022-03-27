de7fdb96a78de221569992ba03e5a945fe3cb235
wszgrcy <wszgrcy@gmail.com>
2022-03-27 18:06:57 +0800

    style: lint

--- src/builder/karma/index.spec.ts
+++ src/builder/karma/index.spec.ts
@@ -1,7 +1,11 @@
+import { BuilderOutput } from '@angular-devkit/architect';
 import { join, normalize } from '@angular-devkit/core';
 import * as fs from 'fs-extra';
 import * as path from 'path';
-import { describeBuilder } from '../../../test/plugin-describe-builder';
+import {
+  BuilderHarnessExecutionResult,
+  describeBuilder,
+} from '../../../test/plugin-describe-builder';
 import {
   DEFAULT_ANGULAR_KARMA_CONFIG,
   KARMA_BUILDER_INFO,
@@ -35,29 +39,27 @@ describeBuilder(runBuilder, KARMA_BUILDER_INFO, (harness) => {
       await harness.host.addSpecEntry(['base-component']);
       harness.useTarget('build', angularConfig);
       let appTestPath: string;
-      const result = new Promise<{
-        result: { success: boolean };
-        error: any;
-        logs: any[];
-      }>((res) => {
-        let result;
-        harness
-          .execute({
-            testContext: {
-              buildSuccess: async (webpackConfig) => {
-                const realTestPath: string = webpackConfig.output!
-                  .path as string;
-                appTestPath = path.resolve(process.cwd(), '__test-app');
-                fs.removeSync(appTestPath);
-                fs.copySync(realTestPath, appTestPath);
+      const result = new Promise<BuilderHarnessExecutionResult<BuilderOutput>>(
+        (res) => {
+          let result;
+          harness
+            .execute({
+              testContext: {
+                buildSuccess: async (webpackConfig) => {
+                  const realTestPath: string = webpackConfig.output!
+                    .path as string;
+                  appTestPath = path.resolve(process.cwd(), '__test-app');
+                  fs.removeSync(appTestPath);
+                  fs.copySync(realTestPath, appTestPath);
+                },
               },
-            },
-          })
-          .subscribe({
-            next: (value) => (result = value),
-            complete: () => res(result),
-          });
-      });
+            })
+            .subscribe({
+              next: (value) => (result = value),
+              complete: () => res(result),
+            });
+        }
+      );
       await result;
       expect((await result).error).toBe(undefined);
       expect((await result).result.success).toBe(true);
@@ -85,37 +87,35 @@ describeBuilder(runBuilder, KARMA_BUILDER_INFO, (harness) => {
       await harness.host.addSpecEntry(['base-component']);
       harness.useTarget('build', { ...angularConfig, watch: true });
       let appTestPath: string;
-      const result = new Promise<{
-        result: { success: boolean };
-        error: any;
-        logs: any[];
-      }>((res) => {
-        let first = true;
-        harness
-          .execute({
-            testContext: {
-              buildSuccess: async (webpackConfig) => {
-                const realTestPath: string = webpackConfig.output!
-                  .path as string;
-                appTestPath = path.resolve(process.cwd(), '__test-app');
-                if (!first) {
+      const result = new Promise<BuilderHarnessExecutionResult<BuilderOutput>>(
+        (res) => {
+          let first = true;
+          harness
+            .execute({
+              testContext: {
+                buildSuccess: async (webpackConfig) => {
+                  const realTestPath: string = webpackConfig.output!
+                    .path as string;
+                  appTestPath = path.resolve(process.cwd(), '__test-app');
+                  if (!first) {
+                    fs.removeSync(appTestPath);
+                    fs.copySync(realTestPath, appTestPath);
+                    return;
+                  }
+                  first = false;
                   fs.removeSync(appTestPath);
                   fs.copySync(realTestPath, appTestPath);
-                  return;
-                }
-                first = false;
-                fs.removeSync(appTestPath);
-                fs.copySync(realTestPath, appTestPath);
-                setTimeout(async () => {
-                  await writeFile();
-                }, 0);
+                  setTimeout(async () => {
+                    await writeFile();
+                  }, 0);
+                },
               },
-            },
-          })
-          .subscribe({
-            next: (value) => res(value as any),
-          });
-      });
+            })
+            .subscribe({
+              next: (value) => res(value),
+            });
+        }
+      );
       await result;
       expect((await result).error).toBe(undefined);
       expect((await result).result.success).toBe(true);
