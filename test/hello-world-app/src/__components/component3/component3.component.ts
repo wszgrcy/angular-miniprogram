3494fae3f67a8d47506b152ee6fd8aa92f5b5dc5
wszgrcy <wszgrcy@gmail.com>
2022-07-14 21:23:44 +0800

    fix: 提高查找入口精确度

--- test/hello-world-app/src/__components/component3/component3.component.ts
+++ test/hello-world-app/src/__components/component3/component3.component.ts
@@ -4,6 +4,7 @@ import { Component, HostListener, OnInit } from '@angular/core';
   selector: 'app-component3',
   templateUrl: './component3.component.html',
   styleUrls: ['./component3.component.css'],
+  standalone: true,
 })
 export class Component3Component implements OnInit {
   @HostListener('tap', ['$event']) tap1(event) {
