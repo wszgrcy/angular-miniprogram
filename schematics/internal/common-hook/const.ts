452c1eb22559cd1577583e3be3415e696bea7dad
chen <wszgrcy@gmail.com>
2024-03-24 23:37:17 +0800

    chore: 去掉无用代码

--- schematics/internal/common-hook/const.ts
+++
@@ -1,8 +0,0 @@
-import path from 'path';
-
-export const SCHEMATICS_COMMON_LIBRARY_PATH = `src/library/common`;
-export const SCHEMATICS_ANGULAR_COMMON_PATH = `.temp-git/common-hook/packages/common`;
-export const ANGULAR_COMMON_PATH = path.resolve(
-  process.cwd(),
-  SCHEMATICS_ANGULAR_COMMON_PATH
-);
